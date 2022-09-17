0754117939d713da7018f06ac54953acde69e04e
defikabulan <devikabulan8@gmail.com>
2022-09-17 19:54:22 +0700

    Sertifikasi DIPA 2022

---
+++ DIPADefika/app/src/main/java/com/example/dipadefika/ListCashflow.java
@@ -0,0 +1,13 @@
+package com.example.dipadefika;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListCashflow extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_cashflow);
+    }
+}
